51a85e86947a1f02eec5495c9040e9d995371d5d
regtorks1 <reginald.torkonoo@amailtech.org>
2022-06-09 18:17:32 +0000

    Nextview created

--- app/src/main/java/com/example/pizzamax/MainActivity.kt
+++ app/src/main/java/com/example/pizzamax/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.pizzamax
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
@@ -17,6 +18,7 @@ import com.example.pizzamax.model.SliderData
 import com.example.pizzamax.views.adapters.SliderAdapter
 import com.example.pizzamax.views.adapters.ValuesDealRecyclerAdapter
 import com.example.pizzamax.views.adapters.ViewPagerAdapter
+import com.example.pizzamax.views.ui.CheckoutActivity
 import com.google.android.material.tabs.TabLayout
 import com.smarteist.autoimageslider.SliderView
 
@@ -29,6 +31,10 @@ class MainActivity : AppCompatActivity(){
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.nextView.setOnClickListener{
+            val intent = Intent(this, CheckoutActivity::class.java)
+            startActivity(intent)
+        }
 
 
         imageSlider()
@@ -105,99 +111,6 @@ class MainActivity : AppCompatActivity(){
         sliderView.isAutoCycle = true
         sliderView.startAutoCycle()
     }
-
-
-
-
-
-
-   /*
-
-    fun alertDialog() {
-        val builder = AlertDialog.Builder(this, R.style.CustomAlertDialog).create()
-        val view = layoutInflater.inflate(R.layout.first_alertdialog, null)
-        val button1 = view.findViewById<Button>(R.id.cancel)
-        val button2 = view.findViewById<Button>(R.id.choose1)
-        val button3 = view.findViewById<Button>(R.id.choose2)
-
-        builder.setView(view)
-        button1.setOnClickListener {
-            builder.dismiss()
-        }
-
-        button2.setOnClickListener {
-            alertDialog_a()
-            view.isVisible = false
-        }
-
-        button3.setOnClickListener {
-            alertDialog_b()
-            view.isVisible = false
-        }
-        builder.setCanceledOnTouchOutside(true)
-        builder.show()
-
-    }
-
-    private fun alertDialog_a() {
-        val builder = AlertDialog.Builder(this, R.style.CustomAlertDialog).create()
-        val view1 = layoutInflater.inflate(R.layout.second_alertdialog, null)
-        val cancelCrust = view1.findViewById<Button>(R.id.cancel1)
-        val confirmCrust = view1.findViewById<Button>(R.id.Confirm)
-
-        cancelCrust.setOnClickListener {
-            builder.dismiss()
-        }
-
-        builder.setView(view1)
-        builder.setCanceledOnTouchOutside(true)
-        builder.show()
-
-        confirmCrust.setOnClickListener {
-            returnDialog1()
-            view1.isVisible = false
-
-        }
-
-    }
-
-
-    private fun alertDialog_b() {
-        val builder = AlertDialog.Builder(this, R.style.CustomAlertDialog).create()
-        val view2 = layoutInflater.inflate(R.layout.third_alertdialog, null)
-        val cancelFlavors = view2.findViewById<Button>(R.id.cancel2)
-        val confirmFlavors = view2.findViewById<Button>(R.id.Confirm1)
-
-        confirmFlavors.setOnClickListener {
-            returnDialog2()
-            view2.isVisible = false
-        }
-
-        cancelFlavors.setOnClickListener {
-            builder.dismiss()
-        }
-        builder.setView(view2)
-        builder.setCanceledOnTouchOutside(true)
-        builder.show()
-    }
-
-
-    private fun returnDialog1() {
-        val builder = AlertDialog.Builder(this, R.style.CustomAlertDialog).create()
-        val view = layoutInflater.inflate(R.layout.first_alertdialog, null)
-        builder.setView(view)
-        builder.setCanceledOnTouchOutside(true)
-        builder.show()
-    }
-
-    private fun returnDialog2() {
-        val builder = AlertDialog.Builder(this, R.style.CustomAlertDialog).create()
-        val view = layoutInflater.inflate(R.layout.first_alertdialog, null)
-        builder.setView(view)
-        builder.setCanceledOnTouchOutside(true)
-        builder.show()
-
-    }*/
 
 
 }
