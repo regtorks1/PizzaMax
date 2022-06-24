a28cb1dc822a73f7b7ec8b2aa119653a30652c00
Peacemaker <peacemaker.otoo@amalitech.org>
2022-06-24 07:49:36 +0000

    favorite_list

--- app/src/main/java/com/example/pizzamax/views/adapters/FavoritesAdapter.kt
+++ app/src/main/java/com/example/pizzamax/views/adapters/FavoritesAdapter.kt
@@ -8,7 +8,7 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.bumptech.glide.Glide
-import com.example.pizzamax.databinding.RecyclerListBinding
+import com.example.pizzamax.databinding.FavoriteListBinding
 import com.example.pizzamax.model.Favorites
 
 class FavoritesAdapter(
@@ -18,7 +18,7 @@ class FavoritesAdapter(
     ListAdapter<Favorites, FavoritesAdapter.RecyclerViewHolder>(ListComparator()) {
     //bind the recycler list items
 
-    inner class RecyclerViewHolder(val binding: RecyclerListBinding) :
+    inner class RecyclerViewHolder(val binding: FavoriteListBinding) :
         RecyclerView.ViewHolder(binding.root) {
         @SuppressLint("SetTextI18n")
         fun bind(list: Favorites) {
@@ -32,28 +32,27 @@ class FavoritesAdapter(
     //inflate the List
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
         return RecyclerViewHolder(
-            RecyclerListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            FavoriteListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         )
     }
 
     //bind the model list the recycler list
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
-        val getItemPosition = getItem(position)
-        holder.bind(getItemPosition)
-        Glide.with(holder.itemView.context).load(getItemPosition.imgUrl)
+        val items = getItem(position)
+        holder.bind(items)
+        Glide.with(holder.itemView.context).load(items.imgUrl)
             .into(holder.binding.posterBanner)
         holder.binding.addCart.setOnClickListener {
-            itemClick("Deal ${getItemPosition.id}", getItemPosition.price)
+            itemClick("Deal ${items.id}", items.price)
         }
 
         holder.itemView.setOnClickListener {
-            listeners.viewDetail(getItemPosition)
+            listeners.viewDetail(items)
         }
 
         holder.binding.favoriteHeart.setOnClickListener {
-            listeners.onItemRemoveClick(getItemPosition.id)
-            notifyDataSetChanged()
+            listeners.onItemRemoveClick(items.id)
         }
     }
 
@@ -69,6 +68,7 @@ class FavoritesAdapter(
     }
 
     interface OnFavoriteDetailPage {
+
         fun viewDetail(favorites: Favorites)
         fun onItemRemoveClick(position: Int)
     }
